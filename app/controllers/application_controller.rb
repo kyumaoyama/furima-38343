6739e6d698851b856d251884ef353185e5e01439
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-08-29 16:15:18 +0900

    ログイン、ログアウト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   before_action :basic_auth, if: :production?   #追加部分
   protect_from_forgery with: :exception
   
