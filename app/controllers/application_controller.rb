f004de86b98821a059520ea4132906f700e83803
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-08-29 15:08:11 +0900

    新規登録成功

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?   #追加部分
   protect_from_forgery with: :exception
+  
+  private
+  def configure_permitted_parameters
+   
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password ,:last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 
 
   private
