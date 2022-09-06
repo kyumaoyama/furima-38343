b9bab9dcc4e7675962b00ad4f6dc0659c28c4acd
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-09-06 18:46:57 +0900

    修正2回め

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
   def update
     @item.update(item_params)
     if @item.valid?
-      redirect_to item_path(item_params)
+      redirect_to item_path(@item)
   else
     render 'edit'
    end
