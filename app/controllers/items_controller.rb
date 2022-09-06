e1ecfb099b95e78c08a041099a74e65a4ec5d347
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-09-06 14:27:52 +0900

    show画面移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     render :new
   end
 end
+  
+  def show
+    @item=Item.find(params[:id])
+  end  
 private
 def item_params
   params.require(:item).permit(:image,:name,:explanation,:category_id,:situation_id,:shipping_charge_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
