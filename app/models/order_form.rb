92f4018edb4b8ef02de6c2e0c5e720865ea80260
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-09-12 14:39:04 +0900

    カード保存成功

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
     validates :user_id
     validates :item_id
   #ship_addresモデルのバリデーション
-  validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
@@ -15,5 +15,8 @@ class OrderForm
     validates :token
   end
 
-
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShipAddresses.create(order_id: order.id,postcode: postcode,prefecture_id: prefecture_id,city: city,building: building,phone_number: phone_number,address: address)
+  end
 end
